c5067ab68a86b678c3c00fe577e5a969ceede0f4
ishan_rawat <ishan.rawat@accolitedigital.com>
2024-06-24 05:26:36 +0530

    without jsx

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h3",{id:"heading"},"I am at my starting Line! Only way is up");
+const heading = React.createElement("h1", { id: "heading" }, "Nah! This is my starting line!");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
