afd384c50be7933d6b21b3606c1020d3a39e9b6e
ishanrawat97 <rawat.ishan23@gmail.com>
2024-06-24 18:38:57 +0530

    JSX

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, "Nah! This is my starting line!");
+const heading = (
+    <h1
+        className="head"
+        tabIndex="5"
+    >
+        I am finally at my starting line
+    </h1>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
