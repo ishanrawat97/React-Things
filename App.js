4a406cd2a2703fcf8b271f0331b7f2449305afa3
ishanrawat97 <rawat.ishan23@gmail.com>
2024-06-24 19:37:53 +0530

    3 ways to call React Component inside another

--- App.js
+++ App.js
@@ -1,28 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const elem = <span>React Element</span>;
+const Title = () => (
+    <h1
+        className="head"
+        tabIndex="5"
+    >
+        Namaste All for one!!!
+    </h1>
+);
+
 //React Functional Component
 const HeadingComponent = () => (
     <div>
         <h1 className="head">
+            <Title></Title>
+            <Title />
+            {Title()}
             Starting-a Line-a Priss Uratra!
         </h1>
     </div>
 );
-const title = (
-    <h1
-        className="head"
-        tableIndex="5"
-    >
-        {elem}
-        One for All
-        <HeadingComponent />
-    </h1>
-);
-
 const root = ReactDOM.createRoot(
     document.getElementById("root")
 );
 
-root.render(title);
+root.render(<HeadingComponent />);
