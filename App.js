d9c0de443407f37e4028bf2912d52c54ca5f13fb
ishanrawat97 <rawat.ishan23@gmail.com>
2024-06-24 18:46:11 +0530

    double functional component

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React Element
-const heading = (<h1 className="head">Starting-a line-a</h1>);
-
+const Title = () => (
+    <h1
+        className="head"
+        tableIndex="5"
+    >
+        One for All
+    </h1>
+);
 //React Functional Component
-const HeadingComponent = () => <div><h1 className="head">Starting-a Line-a Priss Uratra!</h1></div>;
+const HeadingComponent = () => (
+    <div>
+        <Title />
+        <h1 className="head">
+            Starting-a Line-a Priss Uratra!
+        </h1>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
