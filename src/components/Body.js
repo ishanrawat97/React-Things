5c963059692aedb14c159c104da293f4ad0db305
ishanrawat97 <rawat.ishan23@gmail.com>
2024-06-25 23:47:50 +0530

    using react hooks to add top restaurant button functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,28 +1,39 @@
 import RestaurantCard from "./RestaurantCard";
 import resObj from "../utils/mockData";
+import { useState } from "react";
+
 const Body = () => {
+    const [listOfRestuarant, setListOfRestaurant] = useState(resObj);
     return (
         <div className="body">
-            <div className="search">
-                Search
+            <div className="filter">
+                <button
+                    className="filter-btn"
+                    onClick={() => {
+                        const filteredList = listOfRestuarant.filter(
+                            (res) => res.info.avgRating > 4.2
+                        );
+                        setListOfRestaurant(filteredList);
+                    }}
+                >
+                    Top rated Restaurants
+                </button>
+                <button
+                    className="filter-btn"
+                    onClick={() => {
+                        setListOfRestaurant(resObj);
+                    }}
+                >
+                    Show All Restaurants
+                </button>
             </div>
             <div className="res-container">
-                {resObj.map(
-                    (
-                        restaurant
-                    ) => (
-                        <RestaurantCard
-                            key={
-                                restaurant
-                                    .info
-                                    .id
-                            }
-                            resData={
-                                restaurant
-                            }
-                        />
-                    )
-                )}
+                {listOfRestuarant.map((restaurant) => (
+                    <RestaurantCard
+                        key={restaurant.info.id}
+                        resData={restaurant}
+                    />
+                ))}
             </div>
         </div>
     );
